04cd3c34854460b9ece3d8e90071ce12b5a7724e
ptakj <ptakjakub004@gmail.com>
2025-09-20 16:25:11 +0200

    offer CRUD generated

--- src/offer/dto/create-offer.dto.ts
+++ src/offer/dto/create-offer.dto.ts
@@ -1,1 +1,86 @@
-export class CreateOfferDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+import { ListingType } from '@prisma/client';
+
+export class CreateOfferDto {
+  @IsUrl()
+  @ApiProperty()
+  link: string;
+
+  @IsNumber()
+  @ApiProperty()
+  price: number;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  footage?: number;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  rooms?: number;
+
+  @IsDate()
+  @ApiProperty()
+  added_at: Date;
+
+  @IsDate()
+  @ApiProperty()
+  udpated_at: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiPropertyOptional()
+  valid_to?: Date;
+
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty()
+  city: string;
+
+  @IsString()
+  @MaxLength(255)
+  @ApiProperty()
+  address: string;
+
+  @IsOptional()
+  @IsEnum(ListingType)
+  @ApiPropertyOptional({ enum: ListingType })
+  type?: ListingType;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  furniture?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  negotiable?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  pets_allowed?: boolean;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  floor?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  elevator?: boolean;
+}
--- src/offer/dto/update-offer.dto.ts
+++ src/offer/dto/update-offer.dto.ts
@@ -1,4 +1,92 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOfferDto } from './create-offer.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+import { ListingType } from '@prisma/client';
 
-export class UpdateOfferDto extends PartialType(CreateOfferDto) {}
+export class UpdateOfferDto {
+  @IsOptional()
+  @IsUrl()
+  @ApiPropertyOptional()
+  link?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiPropertyOptional()
+  price?: number;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  footage?: number;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  rooms?: number;
+
+  @IsOptional()
+  @IsDate()
+  @ApiPropertyOptional()
+  added_at?: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiPropertyOptional()
+  udpated_at?: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiPropertyOptional()
+  valid_to?: Date;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  @ApiPropertyOptional()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  @ApiPropertyOptional()
+  address?: string;
+
+  @IsOptional()
+  @IsEnum(ListingType)
+  @ApiPropertyOptional({ enum: ListingType })
+  type?: ListingType;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  furniture?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  negotiable?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  pets_allowed?: boolean;
+
+  @IsOptional()
+  @IsInt()
+  @ApiPropertyOptional()
+  floor?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional()
+  elevator?: boolean;
+}
